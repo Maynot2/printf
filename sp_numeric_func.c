e919686f08e06c6437966af697b725d1b48f5e39
Maynot2 <paulmanot@gmail.com>
2020-10-24 14:55:20 +0000

    Add function numlen and missing descriptions

--- sp_numeric_func.c
+++ sp_numeric_func.c
@@ -1,5 +1,29 @@
 #include "holberton.h"
 
+/**
+  * numlen - Gives the number of digits in a number.
+  * @n: A number as an integer.
+  *
+  * Return: The number of digit as an integer.
+  *
+  */
+
+int numlen(int n)
+{
+	if (n / 10 == 0)
+		return (1);
+	return (1 + numlen(n / 10));
+}
+
+/**
+  * _pow - Computes the power of a number n to exp.
+  * @n: A number as an integer.
+  * @exp: The exponential as an integer.
+  *
+  * Return: The power of a number as an integer.
+  *
+  */
+
 int _pow(int n, int exp)
 {
 	int total = 1;
