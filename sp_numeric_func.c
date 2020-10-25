92d201b9723ffab254b30789c8c78539936957ce
Maynot2 <paulmanot@gmail.com>
2020-10-25 16:13:20 +0000

    Change functions

--- sp_numeric_func.c
+++ sp_numeric_func.c
@@ -24,10 +24,10 @@ int numlen(int n)
   *
   */
 
-int _pow(int n, int exp)
+int _pow(int n, int  exp)
 {
-	int total = 1;
-	int i;
+	double total = 1;
+	double i;
 
 	for (i = 0; i < exp; i++)
 		total *= n;
@@ -36,32 +36,33 @@ int _pow(int n, int exp)
 }
 
 /**
- * print_d_i - Print a decimal
+ * add_d_i - Add a decimal char to a buffer.
  * @l: A list of elements.
+ * @buffer: A string buffer.
+ * @i: the current position of the buffer.
  *
  * Return: int
  */
 
-int print_d_i(va_list l)
+int add_d_i(va_list l, char *buffer, int i)
 {
 	int n = va_arg(l, int);
 	unsigned int num = n;
-	int i;
+	int j;
 
 	if (n < 0)
 	{
-		_putchar('-');
+		buffer[i] = ('-');
 		num = -n;
-
+		i++;
 	}
 
 
-	for (i = numlen(num) - 1; i > 0; i--)
+	for (j = 0; j < numlen(n); j++)
 	{
-		_putchar((num / _pow(10, i)) + '0');
-		num -=  (num / _pow(10, i)) * _pow(10, i);
+		buffer[i + j] = (num / _pow(10, numlen(num) - 1)) + '0';
+		num -= (num / _pow(10, numlen(num) - 1)) * _pow(10, numlen(num) - 1);
 	}
-	_putchar(num % 10 + '0');
 
-	return (numlen(num));
+	return (n < 0 ? numlen(n) + 1 : numlen(n));
 }
