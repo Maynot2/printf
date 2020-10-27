1f0da71fc91ad51d84bc06efb6c277a858a81a4a
aureliewouy <aureliecedia@yahoo.com>
2020-10-27 10:48:40 +0000

    fix betty

--- num_helper_func.c
+++ num_helper_func.c
@@ -4,39 +4,39 @@
   * convert_to_base - convert a number to any given base.
   * @num: The number to be converted.
   * @base: The base used for the convertion.
+  * @maj: If the base should be in maj
   *
   * Return: A pointer to the converted string.
   *
   */
 
-char *convert_to_base(int num, int base, int maj) 
-{ 
+char *convert_to_base(int num, int base, int maj)
+{
 	char encodeup[] = "0123456789ABCDEF";
 	char encodelow[] = "0123456789abcdef";
 
-	char *convertion_ptr; 
+	char *convertion_ptr;
 
 	convertion_ptr = malloc((numlen(num) + 1) * sizeof(char));
 	if (!convertion_ptr)
 		return (NULL);
 
-	*convertion_ptr = '\0'; 
+	*convertion_ptr = '\0';
 
-	do 
-	{
+	do	{
 		if (maj)
 		{
-			*--convertion_ptr = encodeup[num % base]; 
+			*--convertion_ptr = encodeup[num % base];
 			num /= base;
 		}
 		else
 		{
-			*--convertion_ptr = encodelow[num % base]; 
+			*--convertion_ptr = encodelow[num % base];
 			num /= base;
 		}
-	} while (num != 0); 
+	} while (num != 0);
 
-	return(convertion_ptr); 
+	return (convertion_ptr);
 }
 
 /**
