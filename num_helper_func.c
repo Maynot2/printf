a9835006263f0d9109b6c9119a113dce232d153a
Maynot2 <paulmanot@gmail.com>
2020-10-27 10:15:43 +0000

    Add convert_to_base fnuction

--- num_helper_func.c
+++ num_helper_func.c
@@ -1,6 +1,45 @@
 #include "holberton.h"
 
 /**
+  * convert_to_base - convert a number to any given base.
+  * @num: The number to be converted.
+  * @base: The base used for the convertion.
+  *
+  * Return: A pointer to the converted string.
+  *
+  */
+
+char *convert_to_base(int num, int base, int maj) 
+{ 
+	char encodeup[] = "0123456789ABCDEF";
+	char encodelow[] = "0123456789abcdef";
+
+	char *convertion_ptr; 
+
+	convertion_ptr = malloc((numlen(num) + 1) * sizeof(char));
+	if (!convertion_ptr)
+		return (NULL);
+
+	*convertion_ptr = '\0'; 
+
+	do 
+	{
+		if (maj)
+		{
+			*--convertion_ptr = encodeup[num % base]; 
+			num /= base;
+		}
+		else
+		{
+			*--convertion_ptr = encodelow[num % base]; 
+			num /= base;
+		}
+	} while (num != 0); 
+
+	return(convertion_ptr); 
+}
+
+/**
  * numlen - Gives the number of digits in a number.
  * @n: A number as an integer.
  * Return: The number of digit as an integer.
@@ -9,9 +48,9 @@
 
 int numlen(int n)
 {
-        if (n / 10 == 0)
-                return (1);
-        return (1 + numlen(n / 10));
+	if (n / 10 == 0)
+		return (1);
+	return (1 + numlen(n / 10));
 }
 
 /**
@@ -26,11 +65,11 @@ int numlen(int n)
 
 int _pow(int n, int  exp)
 {
-        int total = 1;
-        int i;
+	int total = 1;
+	int i;
 
-        for (i = 0; i < exp; i++)
-                total *= n;
+	for (i = 0; i < exp; i++)
+		total *= n;
 
-        return (total);
+	return (total);
 }
