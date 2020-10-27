b469a7fae5e8d5583127e2f60a855de5e3abcbef
Maynot2 <paulmanot@gmail.com>
2020-10-27 10:07:45 +0000

    Remove array of specifiers from _printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,32 @@
 #include "holberton.h"
 #include <unistd.h>
 
+int (*get_format_func(char s))(va_list, char *, int)
+{
+	int i;
+
+	fs_t fs_to_f[] = {
+		{'c', add_c},
+		{'s', add_s},
+		{'i', add_d_i},
+		{'d', add_d_i},
+		{'%', add_percent},
+		{'b', add_binary},
+		{'o', add_oct},
+		{'\0', NULL}
+	};
+
+	i = 0;
+	while (fs_to_f[i].fs)
+	{
+		if (fs_to_f[i].fs == s)
+			return (fs_to_f[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
+
 /**
  * _printf - Like printf
  * @format: The string to print.
@@ -16,33 +42,20 @@ int _printf(const char *format, ...)
 	int i, j, cc;
 	char buff[1024] = {0};
 	va_list list;
-	fs_t s_to_p[] = {
-		{'c', add_c},
-		{'s', add_s},
-		{'i', add_d_i},
-		{'d', add_d_i},
-		{'%', add_percent},
-		{'b', add_binary},
-		{'o', add_oct},
-		{'\0', NULL}
-	};
+
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	cc = 0;
+
 	va_start(list, format);
+
+	cc = 0;
 	i = 0;
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			j = 0;
-			while (s_to_p[j].fs)
-			{
-				if (format[i] == s_to_p[j].fs)
-					cc += s_to_p[j].f(list, buff, cc);
-				j++;
-			}
+			cc += get_format_func(format[i])(list, buff, cc);
 		}
 		else
 		{
